38ed058609937f2eebf47fecafb89453f07b35c7
INRIAS <inrialva@gmail.com>
2023-10-11 22:34:47 -0500

    Js Bucles

---
+++ 06_Repaso/prueba.js
@@ -0,0 +1,22 @@
+let edad = 18;
+
+if (edad >= 18) {
+  console.log("Es mayor de edad");
+} else {
+  console.log("Es menor de edad");
+}
+
+let cantidad = 0;
+let i = 0
+
+while (cantidad <= 5) {
+  console.log("cantidad" + " " + cantidad);
+  cantidad++
+}
+
+let iterar=0
+
+for (let iterar = 0; iterar <= 5; iterar++) {
+  console.log("Iteracion #"+iterar);
+}
+
